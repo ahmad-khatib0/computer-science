ba8d3f439d341fc8940286d63f04ef60f3557b40
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-01-29 20:10:17 +0300

    fundamentals of algorithms

---
+++ data-structure-algorithm/algorithms-4th-ed/fundamentals/app/src/main/java/fundamentals/RandomSeq.java
@@ -0,0 +1,18 @@
+package fundamentals;
+
+package edu.princeton.cs.algs4;
+
+import java.util.Random;
+
+public class RandomSeq {
+  public static void randomSeq(String[] args) {
+    int n = Integer.parseInt(args[0]);
+    double lo = Double.parseDouble(args[1]);
+    double hi = Double.parseDouble(args[2]);
+
+    for (int i = 0; i < n; i++) {
+      double x = StdRandom.uniform(lo, hi);
+      System.out.printf("%.2f\n", x);
+    }
+  }
+}
